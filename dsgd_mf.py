de77ce869dac4a96efb5f492769f55932eb6c588
leoh <c2016.tw@gmail.com>
2015-04-06 21:31:37 -0400

    add dsgd_mf.py, gen_train_input.py

---
+++ dsgd_mf.py
@@ -0,0 +1,15 @@
+import sys 
+from sys import argv 
+
+
+
+if __name__ == "__init__":
+    num_factors = int(argv[1])
+    num_workers = int(argv[2])
+    num_iterations = int(argv[3])
+    beta_value = float(argv[4]) # ?
+    lambda_value = argv[5] 
+    inputV_filepath = argv[6]
+    outputW_filepath = argv[7]
+    outputH_filepath = argv[8]
+
