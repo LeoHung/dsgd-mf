060ccba4f490dcdaa626337aa8b0095de2e82106
Leo Hung <c2016.tw@gmail.com>
2015-04-11 10:36:02 -0400

    update gen_train_input.py

--- gen_train_input.py
+++ gen_train_input.py
@@ -1,6 +1,5 @@
 import os
 import os.path
-import re 
 
 
 def gen(input, output_f):
@@ -10,20 +9,21 @@ def gen(input, output_f):
     for line in f:
         tmp = line.split(",")
         user_id, rating = int(tmp[0]), int(tmp[1])
-        print >> output_f, "%d,%d,%d" %(user_id, movie_id, rating)
+        print >> output_f, "%d,%d,%d" % (user_id, movie_id, rating)
+
 
 def main(input_dir, output):
     output_f = open(output, "w")
 
     for file in os.listdir(input_dir):
         input_file_path = os.path.join(input_dir, file)
-        gen(input_file_path, output_f)        
+        gen(input_file_path, output_f)
 
     output_f.close()
 
 if __name__ == "__main__":
     from sys import argv
-    tmp_file = argv[2] +".tmp"
-    main(argv[1], tmp_file) 
-    os.system("sort %s > %s" %(tmp_file, argv[2]))
-    os.system("rm %s" %(tmp_file))
+    tmp_file = argv[2] + ".tmp"
+    main(argv[1], tmp_file)
+    os.system("sort %s > %s" % (tmp_file, argv[2]))
+    os.system("rm %s" % (tmp_file))
