c74618c8781c820f17b3b258ede8e5bb0dfafb0d
Leo Hung <c2016.tw@gmail.com>
2015-04-16 14:15:21 -0400

    update LNZSL.py

--- LNZSL.py
+++ LNZSL.py
@@ -3,10 +3,10 @@ from eval_acc import LoadSparseMatrix, LoadMatrix, CalculateError
 
 def main(path, v_file):
     epoch = 0
-    V, select = LoadSparseMatrix(v_file) 
-    print "epoch,error" 
-    while True: 
-        w_file = "%sW_%d.csv" %(path, epoch) 
+    V, select = LoadSparseMatrix(v_file)
+    print "epoch,error"
+    while True:
+        w_file = "%sW_%d.csv" %(path, epoch)
         h_file = "%sH_%d.csv" %(path, epoch)
         if(not (isfile(w_file) and isfile(h_file))):
             break
@@ -16,8 +16,16 @@ def main(path, v_file):
             error = CalculateError(V, W, H, select)
             print "%d,%f" %(epoch, error)
 
-        epoch += 1 
-    
+        epoch += 1
+
+
+def main_2(v_file, w_file, h_file):
+    W = LoadMatrix(w_file)
+    H = LoadMatrix(h_file)
+    V, select = LoadSparseMatrix(v_file)
+
+    print "error: %f" %(CalculateError(V, W, H, select))
+
 if __name__ == "__main__":
     from sys import argv
-    main('/tmp/netflix/',argv[1])
+    main_2(argv[1], argv[2], argv[3])
